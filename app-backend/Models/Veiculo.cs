8d691b2321666e7678a82279b71d57aa9d3ba03f
suepuc <1337804@sga.pucminas.br>
2021-10-10 13:19:40 -0300

    Adiciona model Usuario

---
+++ app-backend/Models/Veiculo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app_backend.Models
+{   
+    [Table("Usuarios")]
+    public class Usuario
+    {   
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Obrigatorio Informar a senha!")]
+        public string senha { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio Informar o email!")]
+        public string email{ get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio Informar o nome!")]    
+        public string nomeDeUsuario { get; set; }
+
+        public bool  estaLogueado { get; set; }
+    }
+}
