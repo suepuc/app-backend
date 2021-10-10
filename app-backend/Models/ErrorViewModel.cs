c9e4b8f1a1d161db8bfd9cea2bca94594751c8aa
suepuc <1337804@sga.pucminas.br>
2021-10-10 12:44:31 -0300

    Add project files.

---
+++ app-backend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace app_backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
