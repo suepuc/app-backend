2c9238c8a5a007befabea3d604becfda6cab7305
suepuc <1337804@sga.pucminas.br>
2021-10-10 17:36:26 -0300

    Adiciona Relacionamento entre Receitas e Usuários

--- app-backend/Models/Usuario.cs
+++ app-backend/Models/Usuario.cs
@@ -13,13 +13,14 @@ namespace app_backend.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required(ErrorMessage = "Obrigatorio Informar a senha!")]
+        [Required(ErrorMessage = "Informe a senha!")]
         public string senha { get; set; }
 
         [Required(ErrorMessage = "Informe o email!")]
         public string email { get; set; }
 
         [Required(ErrorMessage = "Informe o nome!")]
+        [Display(Name = "Nome de usuário")]
         public string nomeDeUsuario { get; set; }
 
         public bool estaLogueado { get; set; } = true;
